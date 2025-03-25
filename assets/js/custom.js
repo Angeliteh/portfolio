b010970a70ef78c4bc772cb54740632ec580fd2f
Tu Nombre <juannangell@outlook.com>
2025-03-25 00:56:14 -0600

    cambio anterior revertido

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,7 +24,7 @@
           
           // Actualizar URL
           const newUrl = isGitHubPages 
-            ? `/portafolio${href}`
+            ? `/portfolio${href}`
             : href;
           window.history.pushState({}, '', newUrl);
         }
